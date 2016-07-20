a3f15b31fdff01fbc82a2226ba51a8f99d550877
Iyanu Adelekan <iyanuadelekan@gmail.com>
2016-07-20 02:44:01 +0100

    Create ApiQuery.java

---
+++ paystackjava/ApiQuery.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+/**
+ * Created by Iyanu on 17/07/2016.
+ */
+public class ApiQuery {
+    public ApiQuery(){
+        newQuery();
+    }
+    private HashMap<String,Object> queryMap = null;
+
+    private void newQuery(){
+        queryMap = new HashMap();
+    }
+    public void putParams(String key, Object value){
+        queryMap.put(key,value);
+    }
+    public HashMap <String,Object> getParams(){
+        return queryMap;
+    }
+}
