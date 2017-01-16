cb32711ba0f427dea90f42c10cfde5b1ba7a1e41
ihunanyachi <thompsonamadi@gmail.com>
2017-01-16 18:04:52 +0100

    Keys Class updated

--- paystackjava/Keys.java
+++ paystackjava/Keys.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 public class Keys {
     
 
-  public static final String KEY_IN_USE = "sk_test_68e7f8ca83829d5c0146ecf75070768b208ef9d5";
-  public static final String TEST_SECRET_KEY = "sk_test_68e7f8ca83829d5c0146ecf75070768b208ef9d5";
-  public static final String TEST_PUBLIC_KEY = "pk_test_5b3c05ce6e41b03fa12cb66090c59f36a65084b3";
+  public static final String KEY_IN_USE = "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+  public static final String TEST_SECRET_KEY = "sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+  public static final String TEST_PUBLIC_KEY = "pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
   public static final String LIVE_SECRET_KEY = "sk_live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
   public static final String LIVE_PUBLIC_KEY = "pk_live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
