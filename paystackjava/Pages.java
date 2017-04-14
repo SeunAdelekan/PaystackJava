76987c4c4f41c6dfcfa4fbe8b52d53efe8edcdc4
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-04-14 21:05:40 +0100

    Refactored code in Pages.java

--- paystackjava/Pages.java
+++ paystackjava/Pages.java
@@ -7,93 +7,131 @@ import java.util.HashMap;
  */
 public class Pages {
 
-    public Pages(){
+    private ApiConnection apiConnection;
+    private ApiQuery apiQuery;
 
-    }
-
-    private ApiConnection apiConnection = null;
-    private ApiQuery apiQuery = null;
-
-    /*
-    The following set of methods aids the sending of API
-    requests for the CREATION of pages
+    /**
+     * Used to create a new page
+     * @param queryMap
+     * @return
      */
-
     public JSONObject createPage(HashMap<String, Object> queryMap)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
-        return apiConnection.connectAndQuery(queryMap);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
+        return this.apiConnection.connectAndQuery(queryMap);
     }
 
+    /**
+     * Used to create a new page
+     * @param query
+     * @return
+     */
     public JSONObject createPage(ApiQuery query)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
-        return apiConnection.connectAndQuery(query);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
+        return this.apiConnection.connectAndQuery(query);
     }
 
+    /**
+     * Used to create a new page
+     * @param name
+     * @param description
+     * @param amount
+     * @return
+     */
     public JSONObject createPage(String name, String description, String amount)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
-        apiQuery = new ApiQuery();
-        apiQuery.putParams("name", name);
-        apiQuery.putParams("description", description);
-        apiQuery.putParams("amount", amount);
-        return apiConnection.connectAndQuery(apiQuery);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_CREATE_PAGE);
+        this.apiQuery = new ApiQuery();
+        this.apiQuery.putParams("name", name);
+        this.apiQuery.putParams("description", description);
+        this.apiQuery.putParams("amount", amount);
+
+        return this.apiConnection.connectAndQuery(this.apiQuery);
     }
 
-    /*
-    The following set of methods aids the sending of API
-    requests for the LISTING of pages
+    /**
+     * Used to list created pages
+     * @param queryMap
+     * @return
      */
-
     public JSONObject listPages(HashMap<String, Object> queryMap)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
-        return apiConnection.connectAndQueryWithGet(queryMap);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
+        return this.apiConnection.connectAndQueryWithGet(queryMap);
     }
 
+    /**
+     * Used to list created pages
+     * @param query
+     * @return
+     */
     public JSONObject listPages(ApiQuery query)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
-        return apiConnection.connectAndQueryWithGet(query);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
+        return this.apiConnection.connectAndQueryWithGet(query);
     }
 
+    /**
+     * Used to list created pages
+     * @param perPage
+     * @param page
+     * @return
+     */
     public JSONObject listPages(int perPage, int page)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
-        apiQuery = new ApiQuery();
-        apiQuery.putParams("perPage", perPage);
-        apiQuery.putParams("page", page);
-        return apiConnection.connectAndQueryWithGet(apiQuery);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_LIST_PAGES);
+        this.apiQuery = new ApiQuery();
+        this.apiQuery.putParams("perPage", perPage);
+        this.apiQuery.putParams("page", page);
+
+        return this.apiConnection.connectAndQueryWithGet(this.apiQuery);
     }
 
-    /*
-    The following set of methods aids the sending of API
-    requests for the FETCHING of pages
+    /**
+     * Used to fetch a page
+     * @param idOrSlug
+     * @return
      */
-
     public JSONObject fetchPage(String idOrSlug)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_FETCH_PAGE + idOrSlug);
-        return apiConnection.connectAndQueryWithGet();
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_FETCH_PAGE + idOrSlug);
+        return this.apiConnection.connectAndQueryWithGet();
     }
 
-    /*
-    The following set of methods aids the sending of API
-    requests for the UPDATING of pages
+    /**
+     * Used to update a page
+     * @param idOrSlug
+     * @param queryMap
+     * @return
      */
-
     public JSONObject updatePage(String idOrSlug, HashMap<String, Object> queryMap)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
-        return apiConnection.connectAndQueryWithPut(queryMap);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
+        return this.apiConnection.connectAndQueryWithPut(queryMap);
     }
 
+    /**
+     * Used to update a page
+     * @param idOrSlug
+     * @param query
+     * @return
+     */
     public JSONObject updatePage(String idOrSlug, ApiQuery query)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
-        return apiConnection.connectAndQueryWithPut(query);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
+        return this.apiConnection.connectAndQueryWithPut(query);
     }
 
+    /**
+     * Used to update a page
+     * @param idOrSlug
+     * @param name
+     * @param description
+     * @param amount
+     * @param active
+     * @return
+     */
     public JSONObject updatePage(String idOrSlug, String name, String description, String amount, boolean active)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
-        apiQuery = new ApiQuery();
-        apiQuery.putParams("name", name);
-        apiQuery.putParams("description", description);
-        apiQuery.putParams("amount", amount);
-        apiQuery.putParams("active", active);
-        return apiConnection.connectAndQueryWithPut(apiQuery);
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_PAGES_UPDATE_PAGE + idOrSlug);
+        this.apiQuery = new ApiQuery();
+        this.apiQuery.putParams("name", name);
+        this.apiQuery.putParams("description", description);
+        this.apiQuery.putParams("amount", amount);
+        this.apiQuery.putParams("active", active);
+
+        return this.apiConnection.connectAndQueryWithPut(this.apiQuery);
     }
 
 }
