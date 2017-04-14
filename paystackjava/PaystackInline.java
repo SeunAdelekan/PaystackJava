73082f301205e64082d1cdc6b2f6c0120a189d91
Iyanu Adelekan <iyanuadelekan@gmail.com>
2017-04-14 21:10:22 +0100

    Refactored code in PaystackInline.java

--- paystackjava/PaystackInline.java
+++ paystackjava/PaystackInline.java
@@ -6,72 +6,99 @@ import java.util.HashMap;
  * Created by Iyanu Adelekan on 19/07/2016.
  */
 public class PaystackInline {
-    public PaystackInline(){
 
-    }
-    private static ApiConnection apiConnection = null;
+    private static ApiConnection apiConnection;
 
-    /*
-    The following set of methods aids the sending of API
-    requests for Paystack Standard
+    /**
+     *
+     * @param queryMap
+     * @return
      */
-
-    public JSONObject paystackStandard(HashMap<String, Object> queryMap)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
-        return apiConnection.connectAndQuery(queryMap);
+    public JSONObject paystackStandard(HashMap<String, Object> queryMap) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
+        return this.apiConnection.connectAndQuery(queryMap);
     }
 
-    public JSONObject paystackStandard(ApiQuery query)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
-        return apiConnection.connectAndQuery(query);
+    /**
+     *
+     * @param query
+     * @return
+     */
+    public JSONObject paystackStandard(ApiQuery query) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
+        return this.apiConnection.connectAndQuery(query);
     }
 
-    public JSONObject paystackStandard(String reference, int amount,
-                                 String email, String plan, String callback_url)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
+    /**
+     *
+     * @param reference
+     * @param amount
+     * @param email
+     * @param plan
+     * @param callback_url
+     * @return
+     */
+    public JSONObject paystackStandard(String reference, int amount, String email, String plan, String callback_url) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_PAYSTACK_STANDARD);
         ApiQuery apiQuery = new ApiQuery();
+
         apiQuery.putParams("reference", reference);
         apiQuery.putParams("amount", amount);
         apiQuery.putParams("email", email);
         apiQuery.putParams("plan", plan);
         apiQuery.putParams("callback_url", callback_url);
+
         return apiConnection.connectAndQuery(apiQuery);
     }
 
-    /*
-    The following set of methods aids the sending of API
-    requests for  VERIFYING Paystack Standard transactions
+    /**
+     *
+     * @param reference
+     * @return
      */
-
-    public JSONObject verifyTransactions(String reference)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_VERIFY_TRANSACTIONS + reference);
-        return apiConnection.connectAndQueryWithGet();
+    public JSONObject verifyTransactions(String reference) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_VERIFY_TRANSACTIONS.concat(reference));
+        return this.apiConnection.connectAndQueryWithGet();
     }
 
-    /*
-    The following set of methods aids the sending of API
-    requests for charging returning customers with Paystack Standard
+    /**
+     *
+     * @param queryMap
+     * @return
      */
-
-    public JSONObject chargeReturningCustomer(HashMap<String, Object> queryMap)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
-        return apiConnection.connectAndQuery(queryMap);
+    public JSONObject chargeReturningCustomer(HashMap<String, Object> queryMap) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
+        return this.apiConnection.connectAndQuery(queryMap);
     }
 
-    public JSONObject chargeReturningCustomer(ApiQuery query)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
-        return apiConnection.connectAndQuery(query);
+    /**
+     *
+     * @param query
+     * @return
+     */
+    public JSONObject chargeReturningCustomer(ApiQuery query) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
+        return this.apiConnection.connectAndQuery(query);
     }
 
-    public JSONObject chargeReturningCustomer(String authorization_code, String email,
-                                              String amount, String reference)  {
-        apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
+    /**
+     *
+     * @param authorization_code
+     * @param email
+     * @param amount
+     * @param reference
+     * @return
+     */
+    public JSONObject chargeReturningCustomer(String authorization_code, String email, String amount, String reference) {
+        this.apiConnection = new ApiConnection(Definitions.PAYSTACK_INLINE_CHARGE_AUTHORIZATION);
         ApiQuery apiQuery = new ApiQuery();
+
         apiQuery.putParams("authorization_code", authorization_code);
         apiQuery.putParams("email", email);
         apiQuery.putParams("amount", amount);
         apiQuery.putParams("reference", reference);
-        return apiConnection.connectAndQuery(apiQuery);
+
+        return this.apiConnection.connectAndQuery(apiQuery);
     }
 
 }
