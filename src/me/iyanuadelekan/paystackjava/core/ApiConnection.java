67b85ff238246f32ecf4d8ebcf2b2f2f80b70cc2
ibrahimlawal <ibrahim@lawal.me>
2017-09-23 11:07:17 +0100

    Avoid protocol_version error by forcing TLSv1.2 on all connections
    Signed-off-by: ibrahimlawal <ibrahim@lawal.me>

--- src/me/iyanuadelekan/paystackjava/core/ApiConnection.java
+++ src/me/iyanuadelekan/paystackjava/core/ApiConnection.java
@@ -8,7 +8,17 @@ import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.SSLContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import static org.apache.http.conn.ssl.SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 /**
  * @author Iyanu Adelekan on 17/07/2016.
@@ -33,6 +43,29 @@ public class ApiConnection {
         }
 
         this.apiKey = keys.KEY_IN_USE;
+        this.enforceTlsV1point2();
+    }
+
+       private void enforceTlsV1point2() {
+        try {
+            SSLContext sslContext = SSLContexts.custom()
+                    .useTLS()
+                    .build();
+            SSLConnectionSocketFactory f = new SSLConnectionSocketFactory(
+                    sslContext,
+                    new String[]{"TLSv1.2"},
+                    null,
+                    BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(f)
+                    .build();
+            Unirest.setHttpClient(httpClient);
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(ApiConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KeyManagementException ex) {
+            Logger.getLogger(ApiConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
